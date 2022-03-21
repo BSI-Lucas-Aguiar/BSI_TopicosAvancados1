d8a3d5928a501d41941cf5ff8b7907b67c8daf70
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-03-21 00:38:31 -0300

    Criação de Plugins Separa / Junta RGB

---
+++ TESTPlugin_/src/Junta_RGB_.java
@@ -0,0 +1,41 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.WindowManager;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+public class Junta_RGB_  implements PlugIn {
+    public void run(String arg) {
+    	ImagePlus imagemVermelha = WindowManager.getImage("Imagem Vermelha");
+    	ImagePlus imagemVerde = WindowManager.getImage("Imagem Verde");
+    	ImagePlus imagemAzul = WindowManager.getImage("Imagem Azul");
+    	
+    	ImagePlus imagemRGB = IJ.createImage("Imagem RGB", "RGB", imagemVermelha.getWidth(), imagemVermelha.getHeight(), 1);
+    	
+    	ImageProcessor processadorRGB = imagemRGB.getProcessor();
+    	ImageProcessor processadorVermelho = imagemVermelha.getProcessor();
+		ImageProcessor processadorVerde = imagemVerde.getProcessor();
+		ImageProcessor processadorAzul = imagemAzul.getProcessor();
+		
+		
+		
+		//Processamento RGB
+		imagemRGB.show();
+		int vetorRGB[] = new int[3];
+		for(int x = 0; x < processadorRGB.getWidth(); x++) {
+			for(int y = 0; y < processadorRGB.getHeight(); y++) {
+				vetorRGB[0] = processadorVermelho.getPixel(x, y);
+				vetorRGB[1] = processadorVerde.getPixel(x, y);
+				vetorRGB[2] = processadorAzul.getPixel(x, y);
+				processadorRGB.putPixel(x, y, vetorRGB);
+			}
+		}
+		
+		imagemVermelha.hide();		
+		imagemVerde.hide();
+		imagemAzul.hide();
+		
+		imagemRGB.updateAndDraw();
+		
+    }
+}
---
+++ TESTPlugin_/src/Separa_RGB_.java
@@ -0,0 +1,63 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+public class Separa_RGB_ implements PlugIn{
+	public void run(String arg) {
+		//Seleciona a imagem ativa
+		ImagePlus imagem = IJ.getImage();
+		//IJ.showMessage("Imagem selecionada: "+imagem.getTitle());
+		//IJ.run(imagem, "8-bit", "");
+		
+		
+				
+		//Cria as imagens que irão receber a divisão da imagem principal
+		ImagePlus imagemVermelha = IJ.createImage("Imagem Vermelha", "8-bit", imagem.getWidth(), imagem.getHeight(), 1);
+		ImagePlus imagemVerde = IJ.createImage("Imagem Verde", "8-bit", imagem.getWidth(), imagem.getHeight(), 1);
+		ImagePlus imagemAzul = IJ.createImage("Imagem Azul", "8-bit", imagem.getWidth(), imagem.getHeight(), 1);
+		
+		//Cria os Processadores para a imagem original e para as imagens divididas
+		ImageProcessor processador = imagem.getProcessor();
+		ImageProcessor processadorVermelho = imagemVermelha.getProcessor();
+		ImageProcessor processadorVerde = imagemVerde.getProcessor();
+		ImageProcessor processadorAzul = imagemAzul.getProcessor();
+		
+		
+		//Processamento Vermelho (Red)
+		imagemVermelha.show();
+		for(int x = 0; x < processadorVermelho.getWidth(); x++) {
+            for(int y = 0; y < processadorVermelho.getHeight(); y++) {
+            	processadorVermelho.putPixelValue(x, y, processador.getPixel(x, y, null) [0]);
+            }
+        }
+		//LUT Vermelho
+		IJ.run(imagemVermelha, "Red", "");
+		imagemVermelha.updateAndDraw();
+		
+		//Processamento Verde (Green)
+		imagemVerde.show();
+		for(int x = 0; x < processadorVerde.getWidth(); x++) {
+            for(int y = 0; y < processadorVerde.getHeight(); y++) {
+            	processadorVerde.putPixelValue(x, y, processador.getPixel(x, y, null) [1]);
+            }
+        }
+		//LUT Verde
+		IJ.run(imagemVerde, "Green", "");
+		imagemVerde.updateAndDraw();
+		
+		//Processamento Azul (Blue)
+		imagemAzul.show();
+		for(int x = 0; x < processadorAzul.getWidth(); x++) {
+            for(int y = 0; y < processadorAzul.getHeight(); y++) {
+            	processadorAzul.putPixelValue(x, y, processador.getPixel(x, y, null) [2]);
+            }
+        }
+		//LUT Azul
+		IJ.run(imagemAzul, "Blue", "");
+		imagemAzul.updateAndDraw();
+		
+		imagem.hide();
+		
+	}
+}
