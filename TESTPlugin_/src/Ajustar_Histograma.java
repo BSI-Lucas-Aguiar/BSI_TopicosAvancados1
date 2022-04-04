b191fce5912aeb708bd457366a84687ec8a986e0
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-04-04 14:14:39 -0300

    Update

--- TESTPlugin_/src/Ajustar_Histograma.java
+++ TESTPlugin_/src/Ajustar_Histograma.java
@@ -15,7 +15,7 @@ public class Ajustar_Histograma implements PlugIn, DialogListener {
 	int valorLow = 255;
 	int valorHigh = 0;
 	
-	
+	int vetExpansao[] = new int[256];
 	
 	public void run(String arg) {
 		
@@ -24,7 +24,24 @@ public class Ajustar_Histograma implements PlugIn, DialogListener {
 		ImagePlus imagem = IJ.getImage();
 		ImageProcessor processador = imagem.getProcessor();
 		
+		ImagePlus imagemNova = IJ.createImage("Imagem Nova Cinza", "8-bit", imagem.getWidth(), imagem.getHeight(), 1);
+		ImageProcessor processadorNova = imagemNova.getProcessor();
 		
+		//Cópia da imagem RGB para escala de cinza na imagem 8-bit
+		int mediaRGB = 0;
+		int vetorRGB[] = new int[3];
+		for(int x = 0; x < processador.getWidth(); x++) {
+			for(int y = 0; y < processador.getHeight(); y++) {
+				vetorRGB[0] = processador.getPixel(x, y, null) [0];
+				vetorRGB[1] = processador.getPixel(x, y, null) [1];
+				vetorRGB[2] = processador.getPixel(x, y, null) [2];
+					
+				mediaRGB = (vetorRGB[0]+vetorRGB[1]+vetorRGB[2])/3;
+							
+				processadorNova.putPixel(x, y, mediaRGB);
+			}
+		}
+		imagemNova.show();
 		
 		
 		
@@ -44,25 +61,6 @@ public class Ajustar_Histograma implements PlugIn, DialogListener {
 					
 					//Verifica tipo de imagem RGB, caso seja RGB cria uma nova imagem 8-bit
 					if(imagem.getType() == ImagePlus.COLOR_RGB) {
-						ImagePlus imagemNova = IJ.createImage("Imagem Nova Cinza", "8-bit", imagem.getWidth(), imagem.getHeight(), 1);
-						ImageProcessor processadorNova = imagemNova.getProcessor();
-						
-						//Cópia da imagem RGB para escala de cinza na imagem 8-bit
-						int mediaRGB = 0;
-						int vetorRGB[] = new int[3];
-						for(int x = 0; x < processador.getWidth(); x++) {
-							for(int y = 0; y < processador.getHeight(); y++) {
-								vetorRGB[0] = processador.getPixel(x, y, null) [0];
-								vetorRGB[1] = processador.getPixel(x, y, null) [1];
-								vetorRGB[2] = processador.getPixel(x, y, null) [2];
-									
-								mediaRGB = (vetorRGB[0]+vetorRGB[1]+vetorRGB[2])/3;
-											
-								processadorNova.putPixel(x, y, mediaRGB);
-							}
-						}
-						imagemNova.show();
-						
 						//Chamada da Expansão
 						expansao(processadorNova);
 						imagemNova.updateAndDraw();
@@ -84,9 +82,9 @@ public class Ajustar_Histograma implements PlugIn, DialogListener {
 		
 		
 	}
-	//Irá receber sempre uma imagem cinza, caso a imagem selecionada seja RGB será criada uma nova
-	public void expansao(ImageProcessor processador) {
-		//Valor Low e High
+	
+	//Valor Low e High
+	public void verificaMaiorMenor(ImageProcessor processador) {
 		for(int x = 0; x < processador.getWidth(); x++) {
 			for(int y = 0; y < processador.getHeight(); y++){
 				if(processador.getPixel(x, y) <= valorLow) {
@@ -97,10 +95,18 @@ public class Ajustar_Histograma implements PlugIn, DialogListener {
 				}
 			}
 		}
-		
-		
-		
-		
+	}
+	//Irá receber sempre uma imagem cinza, caso a imagem selecionada seja RGB será criada uma nova
+	public void expansao(ImageProcessor processador) {
+		verificaMaiorMenor(processador);
+		int resultado = 0;
+		for (int x = 0; x < processador.getWidth(); x++) {
+			for (int y = 0; y < processador.getHeight(); y++) {
+				resultado = (valorMin + (processador.getPixel(x, y) - valorLow) * ((valorMax - valorMin)/ (valorHigh - valorLow)));
+				processador.putPixel(x, y, resultado);
+			}
+		}
+
 	}
 	
 	public void equalizacao() {
