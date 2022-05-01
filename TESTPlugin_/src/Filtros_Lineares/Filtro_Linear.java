09b387a34467ef80c738d9c7a764d8706d3f385c
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-05-01 17:38:59 -0300

    Filtros Lineares Update

--- TESTPlugin_/src/Filtros_Lineares/Filtro_Linear.java
+++ TESTPlugin_/src/Filtros_Lineares/Filtro_Linear.java
@@ -13,13 +13,14 @@ public class Filtro_Linear implements PlugIn{
 	ImagePlus imagemGerada = IJ.createImage("Imagem com filtro", "8-bit",imagemOriginal.getWidth(), imagemOriginal.getHeight(), 1);
 	ImageProcessor processadorGerado = imagemGerada.getProcessor();
 	
+	//Vetor para Busca dos pixels ao redor
+	int[] vetor = new int[9];
+	
 	public void run(String arg) {
 		GenericDialog interfaceGrafica = new GenericDialog("Aplicação de Filtros Lineares");
 		String[] selecaoImagens = {"Passa Baixas", "Passa Altas","Borda"};
 		interfaceGrafica.addRadioButtonGroup("Selecione o tipo de Filtro:", selecaoImagens, 1, 3,  "Passa Baixas");
 		interfaceGrafica.showDialog();
-		
-		
 		
 		if (interfaceGrafica.wasCanceled()) {
 			IJ.showMessage("Aplicação de filtro cancelada!");
@@ -37,19 +38,31 @@ public class Filtro_Linear implements PlugIn{
 		}
 	}
 	
+	//Retorna o vetor com os valores da matriz, o valor central é deixado por último para cair nos elses utilizados 
+	public void verificaPontos(int x, int y) {
+		vetor[0] = processadorOriginal.getPixel(x-1,y+1);
+     	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
+     	vetor[2] = processadorOriginal.getPixel(x, y+1);
+     	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
+     	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
+     	vetor[5] = processadorOriginal.getPixel(x-1, y);
+     	vetor[6] = processadorOriginal.getPixel(x+1, y);
+     	vetor[7] = processadorOriginal.getPixel(x, y-1);
+     	vetor[8] = processadorOriginal.getPixel(x, y);
+	}
+	
 	public void aplicaBaixa() {
-		int novoValor;
+		int novaMedia;
 		
 		for (int x = 0; x< processadorOriginal.getWidth() - 1; x++) {
             for (int y = 0; y < processadorOriginal.getHeight() - 1; y++) {
-            	novoValor = 0;
-            	for(int k = x-1; k <= x+1; k++) {
-					for(int l = y-1; l <= y+1; l++) {
-						novoValor += processadorOriginal.getPixel(k, l);
-					}
-				}
-            	
-            	processadorGerado.putPixel(x, y, (novoValor / 9));
+            	novaMedia = 0;
+            	verificaPontos(x, y);
+            	for(int z = 0; z < 9; z++) {
+            		novaMedia += vetor[z];
+            	}
+            	//Divisão pelo número de valores somados
+            	processadorGerado.putPixel(x, y, (novaMedia / 9));
               
             }
         }
@@ -65,21 +78,10 @@ public class Filtro_Linear implements PlugIn{
 		for (int x = 0; x < processadorOriginal.getWidth(); x++){
             for (int y = 0; y < processadorOriginal.getHeight(); y++){
             	novoValor = 0;
-            	
-            	vetor[0] = processadorOriginal.getPixel(x-1,y+1);
-	         	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
-	         	vetor[2] = processadorOriginal.getPixel(x, y+1);
-	         	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
-	         	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
-	         	vetor[5] = processadorOriginal.getPixel(x-1, y);
-	         	vetor[6] = processadorOriginal.getPixel(x+1, y);
-	         	vetor[7] = processadorOriginal.getPixel(x, y-1);
-	         	vetor[8] = processadorOriginal.getPixel(x, y);
+            	verificaPontos(x, y);
 	         	
 	         	for (int i = 0; i < 9; i++){
-	         		if (i == 0){
-	         			novoValor = novoValor + (vetor[i] * -1);
-	         		}else if(i > 0 && i < 8){
+	         		if(i >= 0 && i < 8){
 	         			novoValor = novoValor + (vetor[i] * -1);
 	         		}else{
 	         			novoValor = novoValor + (vetor[i] * 9);
@@ -93,20 +95,11 @@ public class Filtro_Linear implements PlugIn{
 	
 	public void aplicaBorda() {
 		int novoValor;
-		int[] vetor = new int[9];
       
         for (int x = 0; x< processadorOriginal.getWidth(); x++) {
             for (int y = 0; y < processadorOriginal.getHeight(); y++) {
             	novoValor = 0;
-	         	vetor[0] = processadorOriginal.getPixel(x-1,y+1);
-	         	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
-	         	vetor[2] = processadorOriginal.getPixel(x, y+1);
-	         	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
-	         	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
-	         	vetor[5] = processadorOriginal.getPixel(x-1, y);
-	         	vetor[6] = processadorOriginal.getPixel(x+1, y);
-	         	vetor[7] = processadorOriginal.getPixel(x, y-1);
-	         	vetor[8] = processadorOriginal.getPixel(x, y);
+            	verificaPontos(x, y);
          	   
 	         	for (int i = 0; i < 9; i++) {
 	         		if (i < 3) {
--- TESTPlugin_/src/Filtros_Lineares/Filtros_Nao_Lineares.java
+++ TESTPlugin_/src/Filtros_Lineares/Filtros_Nao_Lineares.java
@@ -15,22 +15,20 @@ public class Filtros_Nao_Lineares implements PlugIn{
 	ImagePlus imagemGerada = IJ.createImage("Imagem com filtro", "8-bit",imagemOriginal.getWidth(), imagemOriginal.getHeight(), 1);
 	ImageProcessor processadorGerado = imagemGerada.getProcessor();
 	
+	int[] vetor = new int[9];
+	
 	public void run(String arg) {
 		GenericDialog interfaceGrafica = new GenericDialog("Aplicação de Filtros Não Lineares");
-		String[] selecaoImagens = {"Média", "Moda","Mediana"};
-		interfaceGrafica.addRadioButtonGroup("Selecione o tipo de Filtro:", selecaoImagens, 1, 3,  "Média");
+		String[] selecaoImagens = {"Moda","Mediana"};
+		interfaceGrafica.addRadioButtonGroup("Selecione o tipo de Filtro:", selecaoImagens, 1, 2,  "Moda");
 		interfaceGrafica.showDialog();
-		
-		
 		
 		if (interfaceGrafica.wasCanceled()) {
 			IJ.showMessage("Aplicação de filtro cancelada!");
 		}else{
 			if(interfaceGrafica.wasOKed()) {
 				String opcaoSelecionada = interfaceGrafica.getNextRadioButton();
-				if(opcaoSelecionada == "Média") {
-					aplicaMedia();
-				}else if(opcaoSelecionada == "Moda") {
+				if(opcaoSelecionada == "Moda") {
 					aplicaModa();
 				}else if(opcaoSelecionada == "Mediana") {
 					aplicaMediana();
@@ -39,21 +37,25 @@ public class Filtros_Nao_Lineares implements PlugIn{
 		}
 	}
 	
+	public void verificaPontos(int x, int y) {
+		vetor[0] = processadorOriginal.getPixel(x-1,y+1);
+     	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
+     	vetor[2] = processadorOriginal.getPixel(x, y+1);
+     	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
+     	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
+     	vetor[5] = processadorOriginal.getPixel(x-1, y);
+     	vetor[6] = processadorOriginal.getPixel(x+1, y);
+     	vetor[7] = processadorOriginal.getPixel(x, y-1);
+     	vetor[8] = processadorOriginal.getPixel(x, y);
+	}
+	
 	private void aplicaMediana() {
-		int[] vetor = new int[9];
+		
 		double median;
 		
 		for (int x = 1; x< processadorOriginal.getWidth()-1; x++) {
             for (int y = 1; y < processadorOriginal.getHeight()-1; y++) {
-            	vetor[0] = processadorOriginal.getPixel(x-1,y+1);
-	         	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
-	         	vetor[2] = processadorOriginal.getPixel(x, y+1);
-	         	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
-	         	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
-	         	vetor[5] = processadorOriginal.getPixel(x-1, y);
-	         	vetor[6] = processadorOriginal.getPixel(x+1, y);
-	         	vetor[7] = processadorOriginal.getPixel(x, y-1);
-	         	vetor[8] = processadorOriginal.getPixel(x, y);
+            	verificaPontos(x, y);
 	         	//Ordena o vetor divide por 2 e pega ultimo elemento, para obter o valor do meio
 	         	Arrays.sort(vetor);
 	         	if (vetor.length % 2 == 0) {
@@ -74,15 +76,7 @@ public class Filtros_Nao_Lineares implements PlugIn{
 		
 		for (int x = 1; x< processadorOriginal.getWidth()-1; x++) {
             for (int y = 1; y < processadorOriginal.getHeight()-1; y++) {
-            	vetor[0] = processadorOriginal.getPixel(x-1,y+1);
-	         	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
-	         	vetor[2] = processadorOriginal.getPixel(x, y+1);
-	         	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
-	         	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
-	         	vetor[5] = processadorOriginal.getPixel(x-1, y);
-	         	vetor[6] = processadorOriginal.getPixel(x+1, y);
-	         	vetor[7] = processadorOriginal.getPixel(x, y-1);
-	         	vetor[8] = processadorOriginal.getPixel(x, y);
+            	verificaPontos(x, y);
 	         	Arrays.sort(vetor);
 	         	//Utilizado para pegar o valor mais presente no vetor
 	         	processadorGerado.putPixel(x, y, buscaModa(vetor));
@@ -110,40 +104,6 @@ public class Filtros_Nao_Lineares implements PlugIn{
 			}
 		}
 		return popular;
-	}
-
-	private void aplicaMedia() {
-		int[] vetor = new int[9];
-		int totalParcial;
-		
-		for (int x = 1; x < processadorOriginal.getWidth()-1; x++) {
-            for (int y = 1; y < processadorOriginal.getHeight()-1; y++) {
-            	totalParcial = 0;
-            	vetor[0] = processadorOriginal.getPixel(x-1,y+1);
-            	totalParcial += vetor[0];
-	         	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
-	         	totalParcial += vetor[1];
-	         	vetor[2] = processadorOriginal.getPixel(x, y+1);
-	         	totalParcial += vetor[2];
-	         	vetor[3] = processadorOriginal.getPixel(x-1, y-1);
-	         	totalParcial += vetor[3];
-	         	vetor[4] = processadorOriginal.getPixel(x+1,y-1);
-	         	totalParcial += vetor[4];
-	         	vetor[5] = processadorOriginal.getPixel(x-1, y);
-	         	totalParcial += vetor[5];
-	         	vetor[6] = processadorOriginal.getPixel(x+1, y);
-	         	totalParcial += vetor[6];
-	         	vetor[7] = processadorOriginal.getPixel(x, y-1);
-	         	totalParcial += vetor[7];
-	         	vetor[8] = processadorOriginal.getPixel(x, y);
-	         	totalParcial += vetor[8];
-	         	totalParcial = totalParcial / 9;
-	         	
-	         	processadorGerado.putPixel(x, y, totalParcial);
-              
-            }
-        }
-		imagemGerada.show();
 	}
 
 }
