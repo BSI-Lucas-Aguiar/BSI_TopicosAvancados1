5f54ddc8d872dbbb078383cda75cdf23ce45d8a0
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-05-10 21:26:01 -0300

    Update

--- TESTPlugin_/src/Tecnicas_Morfologicas.java
+++ TESTPlugin_/src/Tecnicas_Morfologicas.java
@@ -6,7 +6,7 @@ import ij.process.ImageProcessor;
 
 public class Tecnicas_Morfologicas implements PlugIn {
 
-	ImagePlus imagemOriginal = IJ.openImage("C:/Users/lukas/Desktop/BSI_TopicosAvancados1/TESTPlugin_/Imagens/sendrak.jpg");
+	ImagePlus imagemOriginal = IJ.getImage();
 	ImageProcessor processadorOriginal = imagemOriginal.getProcessor();
 	ImagePlus imagemEditada = IJ.createImage("Imagem Editada", "8-bit", imagemOriginal.getWidth(), imagemOriginal.getHeight(), 1);
 	ImageProcessor processadorEditado = imagemEditada.getProcessor();
@@ -25,7 +25,6 @@ public class Tecnicas_Morfologicas implements PlugIn {
 			IJ.showMessage("Aplicação de técnica cancelada!");
 		}else{
 			if(interfaceGrafica.wasOKed()) {
-				imagemEditada.show();
 				String opcaoSelecionada = interfaceGrafica.getNextRadioButton();
 				if(opcaoSelecionada == "Dilatação") {
 					percorreImagem(opcaoSelecionada);
@@ -42,6 +41,78 @@ public class Tecnicas_Morfologicas implements PlugIn {
 		}
 	}
 	
+	public void percorreImagem(String opcao) {
+		
+		//Dilatação
+		if(opcao == "Dilatação") {
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaDilatacao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			
+		//Erosão
+		}else if(opcao == "Erosão") {
+			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaErosao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			
+		//Fechamento
+		}else if(opcao == "Fechamento") {
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaDilatacao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			processadorOriginal = processadorEditado.duplicate();
+			
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaErosao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			
+		//Abertura
+		}else if(opcao == "Abertura") {
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaErosao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			processadorOriginal = processadorEditado.duplicate();
+			
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaDilatacao(x, y, processadorEditado, "editado");
+	            }
+			}
+			imagemOriginal.setProcessor(processadorEditado);
+			imagemOriginal.updateAndDraw();
+			
+		//Borda
+		}else if(opcao == "Borda") {
+			for (int x = 0; x < processadorOriginal.getWidth(); x++) {
+	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
+	            	realizaErosao(x, y, processadorEditado, "editado");
+	            }
+			}
+	        realizaBorda();
+			imagemOriginal.updateAndDraw();
+		}
+	}
+	
 	public void verificaPontos(int x, int y) {
 		vetor[0] = processadorOriginal.getPixel(x-1,y+1);
      	vetor[1] = processadorOriginal.getPixel(x+1,y+1);
@@ -54,104 +125,71 @@ public class Tecnicas_Morfologicas implements PlugIn {
      	vetor[8] = processadorOriginal.getPixel(x, y);
 	}
 	
-	public void percorreImagem(String opcao) {
-		if(opcao == "Dilatação") {
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaDilatacao(x, y);
-	            }
-			}
-			imagemEditada.updateAndDraw();
-		}else if(opcao == "Erosão") {
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaErosao(x, y);
-	            }
-			}
-			imagemEditada.updateAndDraw();
-		}else if(opcao == "Fechamento") {
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaDilatacao(x, y);
-	            	
-	            }
-			}
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaErosao(x, y);
-	            	
-	            }
-			}
-			imagemEditada.updateAndDraw();
-		}else if(opcao == "Abertura") {
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaErosao(x, y);
-	            }
-			}
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaDilatacao(x, y);
-	            }
-			}
-			imagemEditada.updateAndDraw();
-		}else if(opcao == "Borda") {
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaErosao(x, y);
-	            	
-	            }
-			}
-			for (int x = 0; x< processadorOriginal.getWidth(); x++) {
-	            for (int y = 0; y < processadorOriginal.getHeight(); y++) {
-	            	realizaBorda(x, y);
-	            	
-	            }
-			}
-			
-			imagemEditada.updateAndDraw();
+	public void verificaPontosEditado(int x, int y) {
+		vetor[0] = processadorEditado.getPixel(x-1,y+1);
+     	vetor[1] = processadorEditado.getPixel(x+1,y+1);
+     	vetor[2] = processadorEditado.getPixel(x, y+1);
+     	vetor[3] = processadorEditado.getPixel(x-1, y-1);
+     	vetor[4] = processadorEditado.getPixel(x+1,y-1);
+     	vetor[5] = processadorEditado.getPixel(x-1, y);
+     	vetor[6] = processadorEditado.getPixel(x+1, y);
+     	vetor[7] = processadorEditado.getPixel(x, y-1);
+     	vetor[8] = processadorEditado.getPixel(x, y);
+	}
+	
+	public void realizaDilatacao(int x, int y, ImageProcessor processadorQueVaiSerEditado, String qual) {
+		verificaPontos(x, y);
+    	if(vetor[8] == 0) {
+    		processadorQueVaiSerEditado.putPixel(x-1,y+1, 0);
+    		processadorQueVaiSerEditado.putPixel(x+1,y+1, 0);
+    		processadorQueVaiSerEditado.putPixel(x, y+1, 0);
+    		processadorQueVaiSerEditado.putPixel(x-1, y-1, 0);
+    		processadorQueVaiSerEditado.putPixel(x+1,y-1, 0);
+    		processadorQueVaiSerEditado.putPixel(x-1, y, 0);
+    		processadorQueVaiSerEditado.putPixel(x+1, y, 0);
+    		processadorQueVaiSerEditado.putPixel(x, y-1, 0);
+    		processadorQueVaiSerEditado.putPixel(x, y, 0);    			
+    	}
+    	
+    	if(qual == "original") {
+			processadorOriginal = processadorQueVaiSerEditado;
+		}else if(qual == "editado") {
+			processadorEditado = processadorQueVaiSerEditado;
 		}
 	}
 	
-
-	
-	public void realizaDilatacao(int x, int y) {
+	public void realizaErosao(int x, int y, ImageProcessor processadorQueVaiSerEditado, String qual) {
 		verificaPontos(x, y);
-    	if(vetor[8] == 0) {
-    		processadorEditado.putPixel(x-1,y+1, 0);
-    		processadorEditado.putPixel(x+1,y+1, 0);
-    		processadorEditado.putPixel(x, y+1, 0);
-    		processadorEditado.putPixel(x-1, y-1, 0);
-    		processadorEditado.putPixel(x+1,y-1, 0);
-    		processadorEditado.putPixel(x-1, y, 0);
-    		processadorEditado.putPixel(x+1, y, 0);
-    		processadorEditado.putPixel(x, y-1, 0);
-    		processadorEditado.putPixel(x, y, 0);    			
-    	}
-	}
-	
-	
-	public void realizaErosao(int x, int y) {
-		verificaPontos(x, y);
-		boolean areaInteresse = true;
+		boolean areaDeInteresse = true;
 		
 		//Caso a área de interesse não seja preta não irá realizar operação
-		for(int i = 0; i < vetor.length; i++) {
+		for(int i = 0; i < 9; i++) {
 			if(vetor[i] != 0) {
-				areaInteresse = false;
+				areaDeInteresse = false;
 			}
 		}
 		//Caso permaneça verdadeira é preenchido na imagem cópia
-		if(areaInteresse) {
-			processadorEditado.putPixel(x, y, 0);
+		if(areaDeInteresse) {
+			processadorQueVaiSerEditado.putPixel(x, y, 0);
+		}
+		if(qual == "original") {
+			processadorOriginal = processadorQueVaiSerEditado;
+		}else if(qual == "editado") {
+			processadorEditado = processadorQueVaiSerEditado;
 		}
 	}
 	
-	
-	public void realizaBorda(int x, int y) {
-		int[] valor = imagemOriginal.getPixel(x, y);
-		if(valor[0] == 0) {
-			processadorEditado.putPixel(x, y, valor);
+	public void realizaBorda() {
+		int imgCopia;
+		
+		for(int x = 0; x < processadorOriginal.getWidth(); x++) {
+			for(int y = 0; y < processadorOriginal.getHeight(); y++) {
+				imgCopia = processadorEditado.getPixel(x, y);
+				
+				if(imgCopia == 0) {
+					processadorOriginal.putPixel(x, y, 255);
+				}
+			}
 		}
 	}
 }
