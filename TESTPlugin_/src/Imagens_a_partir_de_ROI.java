ab963832ee27d5e8ed0331f529905b8a3e447e68
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-05-12 22:14:09 -0300

    Update ROI Completo

--- TESTPlugin_/src/Imagens_a_partir_de_ROI.java
+++ TESTPlugin_/src/Imagens_a_partir_de_ROI.java
@@ -1,68 +1,49 @@
 import ij.IJ;
 import ij.ImagePlus;
+import ij.Prefs;
+import ij.gui.Roi;
 import ij.plugin.PlugIn;
+import ij.plugin.frame.RoiManager;
 
 public class Imagens_a_partir_de_ROI implements PlugIn{
 
 	public void run(String arg) {
 		
-		ImagePlus imp = IJ.openImage("C:/Users/lukas/Desktop/BSI_TopicosAvancados1/TESTPlugin_/Imagens/sprite.jpg");
+		//Aberta automaticamente a imagem e feito todo o processamento
+		ImagePlus imp = IJ.openImage("C:/Users/lukas/Desktop/BSI_TopicosAvancados1/TESTPlugin_/Imagens/Sprite/sprite.jpg");
+		imp.show();
+		
+		//Segunda imagem copiada para pegar o roi corretamente
+		ImagePlus imp2 = imp.duplicate();
+		imp2.show();
+		Prefs.blackBackground = false;
+		IJ.run(imp2, "Convert to Mask", "");
+		IJ.run(imp2, "Fill Holes", "");
 		
 		IJ.run("ROI Manager...", "");
-		Object rm;
-		imp.setRoi(38,28,70,114);
-		rm.addRoi(roi);
-		imp.setRoi(121,30,66,109);
-		rm.addRoi(roi);
-		imp.setRoi(194,30,72,106);
-		rm.addRoi(roi);
-		imp.setRoi(297,33,82,106);
-		rm.addRoi(roi);
-		imp.setRoi(385,32,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(489,32,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(33,156,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(125,156,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(218,153,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(308,153,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(399,156,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(494,157,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(21,304,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(122,295,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(222,279,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(315,277,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(403,292,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(501,303,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(26,429,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(109,430,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(195,429,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(273,431,99,106);
-		rm.addRoi(roi);
-		imp.setRoi(365,427,80,106);
-		rm.addRoi(roi);
-		imp.setRoi(438,423,75,106);
-		rm.addRoi(roi);
-		imp.setRoi(510,422,75,106);
-		rm.addRoi(roi);
+		IJ.run(imp2, "Analyze Particles...", "add");
 		
+		RoiManager roiManager = RoiManager.getRoiManager();
+		Roi[] rm = roiManager.getRoisAsArray();
+		
+		int contador = 0;
+		
+		//Percorre o vetor roi 
+		for (int i = 0; i < rm.length; i++) {
+			contador++;
+			//Define o Roi atual do vetor roi da imagem duplicada na imagem original(colorida)
+			imp.setRoi(rm[i].getBounds());
+			//Recorta com base no roi gerado porém na imagem original para manter as cores
+			ImagePlus imagemSprite = imp.crop();
+			imagemSprite.setTitle("Sprite Recortado " + contador);
+			IJ.saveAs(imagemSprite, "PNG", "C:/Users/lukas/Desktop/BSI_TopicosAvancados1/TESTPlugin_/Imagens/Sprite/"+imagemSprite.getTitle());
+		}
+		
+		IJ.run("Close");
+		imp.changes = false;
+		imp.close();
+		imp2.changes = false;
+		imp2.close();
+		IJ.showMessage("Imagem recortada e sprites separados salvos");
 	}
-	
-	
-
 }
